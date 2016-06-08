f979cb9036e419f246ddf4df27630fcd60cbe2a7
Bruno R <brundows2008@live.cl>
2016-06-08 14:05:22 -0300

    Re-estructuración en base a lo visto en Laboratorio.
    Se ha re-estructurado "Contacto", "NodoRaiz" pasa a ser "NodoInterno".

--- libs/contacto.h
+++ libs/contacto.h
@@ -1,9 +1,10 @@
 #define MAX_TEL_LENGTH 12
+#define LIMIT_NAMES 10
 
 typedef struct contacto
 {
 	int clave;
-	char* nombre;
-	char* apellido;
+	char nombre[LIMIT_NAMES];
+	char apellido[LIMIT_NAMES];
 	int telefono[MAX_TEL_LENGTH];
 } Contacto;
--- libs/nodo_hoja.h
+++ libs/nodo_hoja.h
@@ -1,11 +1,9 @@
 #include <contacto.h>
-#include <lista.h>
 
 typedef struct nodo_h
 {
 	char* nombre;
 	Contacto* contacto[CLAVE_LENGTH];
 	int cantidadClaves;
-	int altura;
 	struct nodo_h* siguiente;
 } NodoHoja;
