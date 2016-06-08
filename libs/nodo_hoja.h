d008bef6bfe52ef620e1b8fda00de479597fd4e7
kurogane19 <hio_matt_18@outlook.com>
2016-06-08 19:33:45 -0400

    merge changes
    inicializado de hoja nula

--- libs/nodo_hoja.h
+++ libs/nodo_hoja.h
@@ -1,5 +1,6 @@
 #include <contacto.h>
 
+//t = 17.
 typedef struct nodo_h
 {
 	char* nombre;
@@ -7,3 +8,20 @@ typedef struct nodo_h
 	int cantidadClaves;
 	struct nodo_h* siguiente;
 } NodoHoja;
+
+/*
+	Nodo (Hoja / Interno)
+		> CrearNodo.
+		> LiberarNodo.
+		> Guardar / Cargar.
+
+*/
+
+NodoHoja *crearHoja(){
+	NodoHoja *auxHoja=malloc(sizeof(NodoHoja));
+	auxHoja->contacto = NULL;
+	auxHoja->nombre = NULL;
+	auxHoja->cantidadClaves = -1;
+	auxHoja->siguiente = NULL;
+	return (auxHoja);
+}
