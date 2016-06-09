3e3ab8164838c4e40cf04a26f00308843fa70ce0
NicolasGarciaSantelices <ngarcia1621@gmail.com>
2016-06-09 12:32:33 -0400

    Gran Update 1.1
    se combinan ambos códigos para la implementación de nodo_interno

--- libs/nodo_hoja.h
+++ libs/nodo_hoja.h
@@ -1,12 +1,12 @@
 #include "contacto.h"
-
-#define NODOHOJA_HEADER 'h'
-
-//t = 17.
+#define NODOHOJA_HEADER 'H'
+#define K 74
+#define HOJA_CLAVES 2*K
 typedef struct nodo_h
 {
+	int clave;
 	char nombre[LONG_FILENAME];
-	Contacto* contacto[HIJOS_LENGTH];
+	Contacto* contactos[HOJA_CLAVES];
 	int cantidadClaves;
 	char siguiente[LONG_FILENAME];
 } NodoHoja;
@@ -32,25 +32,29 @@ void nodohoja_Guardar(NodoHoja** nodo, char* nombre) {
 	if(nodo != NULL) {
 		FILE* file = fopen(nombre, "wb");
 
-		//Cabezal que indicará el tipo de nodo a guardar.
+		//Cabezal que indicará el tipo de nodo a guardar. (6 * 1 byte = 5 bytes)
 		fwrite(NODOHOJA_HEADER, sizeof(char), NODO_HEADER_LENGTH, file);
 
-		//Nombre del nodo.
+		//Nombre del nodo. (5 * 1 byte = 5 bytes)
 		fwrite((*nodo)->nombre, sizeof(char), LONG_FILENAME, file);
 
-		//Cantidad de Claves.
+		//Cantidad de Claves. (4 bytes)
 		fwrite(&(*nodo)->cantidadClaves, sizeof(int), LONG_FILENAME, file);
 
-		//Siguiente Nodo (Nombre).
+		//Siguiente Nodo (Nombre). (5 * 1 byte = 5 bytes)
 		fwrite((*nodo)->siguiente, sizeof(int), LONG_FILENAME, file);
 
-		//Escribiendo cada contacto.
-		for (int i = 0; i < CLAVE_LENGTH; ++i)
+		//Escribiendo cada contacto. 
+		for (int i = 0; i < HIJOS_LENGTH; ++i)
 		{
 			if((*nodo)->contacto[i] != NULL) {
+				//clave (4 bytes)
 				fwrite(&((*nodo)->contacto[i])->clave, sizeof(int), 1, file);
+				//nombre (10 bytes)
 				fwrite(((*nodo)->contacto[i])->nombre, sizeof(char), LIMIT_NAMES, file);
+				//apellido (10 bytes)
 				fwrite(((*nodo)->contacto[i])->apellido, sizeof(char), LIMIT_NAMES, file);
+				//Telefono (4 bytes por el compilador)
 				fwrite(&((*nodo)->contacto[i])->telefono, sizeof(unsigned int), 1, file);
 			}
 		}
@@ -85,16 +89,14 @@ NodoHoja* nodohoja_Cargar(char* nombre) {
 			//Lectura de contactos disponibles.
 			for (int i = 0; i < CLAVE_LENGTH; ++i)
 			{
+				nuevo->contacto[i] = contacto_Crear();
+				
 				//Si se llega al final del archivo.
 				if(!feof(fp)) {
-					//TODO: Crear esto...
-					nuevo->contacto[i] = contacto_Crear();
 					fread(&(nuevo->contacto[i])->clave, sizeof(int), 1, fp);
 					fread((nuevo->contacto[i])->nombre, sizeof(char), LIMIT_NAMES, fp);
 					fread((nuevo->contacto[i])->apellido, sizeof(char), LIMIT_NAMES, fp);
 					fread(&(nuevo->contacto[i])->telefono, sizeof(unsigned int), 1, fp);
-				} else {
-					nuevo->contacto[i] = NULL;
 				}
 			}
 
--- libs/nodo_interno.h
+++ libs/nodo_interno.h
@@ -1,10 +1,14 @@
-#define NODOINTER_HEADER 'i'
+#define NODOINTER_HEADER 'I'
+#define T 226
+#define INTER_CLAVES  2 * T
+#define INTER_HIJOS (INTER_CLAVES + 1)
 
 typedef struct nodo_i
 {
-	char nombre[LONG_FILENAME];
-	int claves[CLAVE_LENGTH];
-	char hijos[HIJOS_LENGTH][LONG_FILENAME];
+	int clave;
+	char nombre[LONG_FILENAME]; 
+	int claves[INTER_CLAVES];
+	char hijos[INTER_HIJOS][LONG_FILENAME];
 	int cantidadClaves;
 } NodoInterno;
 
@@ -58,4 +62,50 @@ NodoInterno nodointer_cargar(char *nombre){
         printf("Imposible cargar el nodo interno: %s\n", nombre);
     }
     return nuevo_nodo;
+}
+
+    
+NodoInterno* nodointer_Crear() {
+	NodoInterno* nuevo = malloc(sizeof(NodoInterno));
+	strcpy(nuevo->nombre, "");
+	for (int i = 0; i < INTER_HIJOS; ++i)
+	{
+		strcpy(nuevo->hijos[i], "");
+	}
+	for (int i = 0; i < INTER_CLAVES; ++i)
+	{
+		nuevo->claves[i] = -1;
+	}
+	nuevo->cantidadClaves = 0;
+	return nuevo;
+}
+
+void nodointer_Guardar(NodoInterno* nodo, char* nombre) {
+	FILE *file = fopen(nombre, "wb");
+
+	if(nodo != NULL) {
+		if(file != NULL) {
+			//Escribiendo tipo de nodo como cabezal. (6 * 1 bytes = 6)
+			fwrite(NODOINTER_HEADER, sizeof(char), NODO_HEADER_LENGTH, file);
+
+			//Nombre del nodo (5 * 1 byte = 5 bytes)
+			fwrite(nodo->nombre, sizeof(char), LONG_FILENAME, file);
+			//Cantidad de Claves (4 bytes)
+			fwrite(&nodo->cantidadClaves, sizeof(int), 1, file);
+			//Claves (2 * 4 bytes = 8 bytes)
+			fwrite(nodo->claves, sizeof(int), INTER_CLAVES, file);
+
+			//Nombre de los hijos (5 * 3 * 1 bytes = 15 bytes)
+			for (int i = 0; i < INTER_HIJOS; ++i)
+			{
+				fwrite(nodo->hijos[i], sizeof(char), LONG_FILENAME, file);
+			}
+
+			fclose(file);
+		} else {
+			printf("Imposible guardar el nodo: %s\n", nombre);
+		}
+	} else {
+		printf("Hombre!, no me puedes entregar un nodo nulo.\n");
+	}
 }
