159aa2b2c14d1807645f971a4f8a05b1cdb6abf5
Bruno R <brundows@users.noreply.github.com>
2016-06-09 11:06:26 -0300

    Actualizando nodo_hoja.h

--- libs/nodo_hoja.h
+++ libs/nodo_hoja.h
@@ -3,10 +3,10 @@
 //t = 17.
 typedef struct nodo_h
 {
-	char* nombre;
+	char nombre[LONG_FILENAME];
 	Contacto* contacto[CLAVE_LENGTH];
 	int cantidadClaves;
-	struct nodo_h* siguiente;
+	char siguiente[LONG_FILENAME];
 } NodoHoja;
 
 /*
@@ -31,13 +31,11 @@ NodoHoja *crearHoja(){
 
 
 
-/*NodoHoja *LibararHoja(NodoHoja *Hojas,int ){
-	if(Hojas == NULL)	return -1;
-	if(Hojas->cantidadClaves == CLAVE_LENGTH){
-		
-	}else{
-		
-	}
+/* int LibararHoja(NodoHoja *Hoja,int ){
+	if(Hojas == NULL) return 0;
+	free(hoja->contacto);
+	free(hoja);
+	return 0;
 }
 */
 
